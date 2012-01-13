d4ba2be17c6854852293e82ef6158a6043e168da
mpl <mathieu.lonjaret@gmail.com>
2012-01-13 18:46:11 +0100

    v1

--- pygtkScp.py
+++ pygtkScp.py
@@ -1,11 +1,8 @@
 import gtk
 import os
 
-def browseCallBack(self, entry=None):
-	browse(entry)
+def browse(self, entry=None):
 	print 'file: ', entry.get_text()
-
-def browse(entry):
 	dialog = gtk.FileChooserDialog("Open..",
 		None,
 		gtk.FILE_CHOOSER_ACTION_OPEN,
@@ -18,87 +15,22 @@ def browse(entry):
 	filter.add_pattern("*")
 	dialog.add_filter(filter)
 
-	filter = gtk.FileFilter()
-	filter.set_name("Images")
-	filter.add_mime_type("image/png")
-	filter.add_mime_type("image/jpeg")
-	filter.add_mime_type("image/gif")
-	filter.add_pattern("*.png")
-	filter.add_pattern("*.jpg")
-	filter.add_pattern("*.gif")
-	filter.add_pattern("*.tif")
-	filter.add_pattern("*.xpm")
-	dialog.add_filter(filter)
-
 	response = dialog.run()
 	if response == gtk.RESPONSE_OK:
-#		print dialog.get_filename(), 'selected'
-		data = dialog.get_filename()
-		entry.set_text(data)
+		entry.set_text(dialog.get_filename())
 	elif response == gtk.RESPONSE_CANCEL:
 		print 'Closed, no files selected'
-		data = ''
 	dialog.destroy()
-	return data
 
-def sourceBox():
-	sBox = gtk.HBox(False, 0)
-	sBox.set_border_width(2)
-
-	label1 = gtk.Label("Browse")
-	button1 = gtk.Button()
-	button1.add(label1)
-	button1.connect("clicked", browseCallBack, "cool button")
-
-	entry = gtk.Entry()
-
-	sBox.pack_start(entry, False, False, 3)
-	sBox.pack_start(button1, False, False, 3)
-	entry.show()
-	label1.show()
-	button1.show()
-
-	return sBox
-
-def mainBox():
-	box0 = gtk.HBox(False, 0)
-	box0.set_border_width(2)
-
-	sBox = sourceBox()
-	dBox = sourceBox()
-
-	box0.pack_start(sBox, False, False, 3)
-	box0.pack_start(dBox, False, False, 3)
-	sBox.show()
-	dBox.show()
-
-	return box0
-
-def mainWindow():
-	# Create a new window
-	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-
-	window.set_title("Scp")
-
-	# It's a good idea to do this for all windows.
-	window.connect("destroy", lambda wid: gtk.main_quit())
-	window.connect("delete_event", lambda a1,a2:gtk.main_quit())
-
-	# Sets the border width of the window.
-	window.set_border_width(10)
-
-	# This calls our box creating function
-	box0 = mainBox()
-	box0.show()
-
-	window.add(box0)
-	window.show()
-
-def doScp(self, source, dest):
-#	try:
-	os.execl("/usr/bin/scp", "scp", source.get_text(), dest.get_text())
-#	except OSError:
-#		print "nope"
+def doScp(self, source, destination):
+	src = source.get_text()
+	dest = destination.get_text()
+	args = ['scp']
+	if os.path.isdir(src):
+		args.append('-r')
+	args.append(src)
+	args.append(dest)
+	os.execv("/usr/bin/scp", args)
 	
 # abject oriented is for wusses, hence do it all in one func like a boss
 def doAll():
@@ -124,7 +56,12 @@ def doAll():
 	button1 = gtk.Button()
 	button1.add(label1)
 	entry1 = gtk.Entry()
-	button1.connect("clicked", browseCallBack, entry1)
+	entry1.set_text("enter source or browse")
+	button1.connect("clicked", browse, entry1)
+	usEntry = gtk.Entry()
+	usEntry.set_text("username")
+	psEntry = gtk.Entry()
+	psEntry.set_text("password")
 	sBox.pack_start(entry1, False, False, 3)
 	sBox.pack_start(button1, False, False, 3)
 	entry1.show()
@@ -137,7 +74,12 @@ def doAll():
 	button2 = gtk.Button()
 	button2.add(label2)
 	entry2 = gtk.Entry()
-	button2.connect("clicked", browseCallBack, entry2)
+	entry2.set_text("enter dest or browse")
+	button2.connect("clicked", browse, entry2)
+	udEntry = gtk.Entry()
+	udEntry.set_text("username")
+	pdEntry = gtk.Entry()
+	pdEntry.set_text("password")
 	dBox.pack_start(entry2, False, False, 3)
 	dBox.pack_start(button2, False, False, 3)
 	entry2.show()
